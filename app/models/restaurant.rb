a6985b4176227119d0d68fa6ca483b5864e3e3b9
github username <github email address>
2020-03-20 12:11:57 -0700

    finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,42 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    arr = []
+    # build link to the review class and push all reviews about this 
+    # restaurant into the array and then return it..
+
+    # review.all.map 
+  end
+
 end
+
+
+
+
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
+# - `Restaurant#customers`
+#   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+
+
+
+
+
+
+
+
+
+
+
+
+#done
+
+##################################
+# #### Restaurant
+
+# - `Restaurant#initialize`
+#   - Restaurants should be initialized with a name, as a string
+# - `Restaurant#name`
+#   - returns the restaurant's name
+#   - should not be able to change after the restaurant is created
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,65 @@
 class Review
-  
+
+    attr_reader :customer, :restaurant, :rating
+
+    @@all = []
+
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer(rating)
+        @@all.each do |s|
+            if s.rating == rating
+               @customer  # what to return (@customer is an instance not an obj)
+            end
+        end
+        # return an obj of this customer's name?
+        # once an obj instance is created for this class the name of the customer 
+        # should not be changed ((attr_reader) stops customer from change)
+    end
+
+    def restaurant(rating)
+        # returns restaurant obj about that rating num (integer)
+    end
+
+    # def rating(restaurant)   # This should work (maybe with the help of another class)
+    #     @@all.select do |s|
+    #         if s.restaurant == restaurant
+    #            var = @rating
+    #         end
+    #     end
+    #     var
+    # end
+
 end
+
+# - `Review#customer`
+#   - returns the customer object for that review
+#   - Once a review is created, should not be able to change the customer
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, should not be able to change the restaurant
+
+
+# - `Review#rating`
+#   - returns the rating for a restaurant.
+
+#done 
+
+##################################
+#### Review
+
+# - `Review#initialize`
+#   - Reviews should be initialized with a customer, restaurant, and a rating (a number)
+
+
+# - `Review.all`
+#   - returns all of the reviews
